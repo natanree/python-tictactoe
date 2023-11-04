488926b83fe2db59e374a001a2f9fa7a258441d7
Yu Chung Lee <yuchung.lee@wsu.edu>
2023-11-03 19:15:22 -0700

    changed TicTacToe.board to be a nested list, implemented display_board_guide, display_board, and get_board_input for TicTacToe

--- app/game.py
+++ app/game.py
@@ -2,9 +2,23 @@ import math
 
 class TicTacToe():
     def __init__(self):
-        self.board = [' ' for space in range(9)]
+        self.board = [[' ' for column in range(3)] for row in range(3)]
         self.winner = None
     
+    def display_board_guide(self):
+        print('| 1 | 2 | 3 |\n| 4 | 5 | 6 |\n| 7 | 8 | 9 |\n')
+
+    def display_board(self):
+        for row in self.board:
+            print('| ' + ' | '.join(row) + ' |')
+
+    def get_board_input(self):
+        self.display_board()
+        val = int(input('\nInput Choice: '))
+        if val < 1 or val > 3:
+            val = int(input('\nInvalid choice! please pick a number from 1 to 9: '))
+        return val 
+
     def main_menu(self):
         val = int(input('1. Play vs person\n2. Play vs AI\n3. Quit\n'))
         if val < 1 or val > 3:
@@ -13,4 +27,4 @@ class TicTacToe():
 
 if __name__ == "__main__":
     game = TicTacToe()
-    val = game.main_menu()
+    val = game.get_board_input()
